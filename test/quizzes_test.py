dc96796bfab86a3b761c6276a6813ab8d16e2815
urstrulykkr <reddykaushik18@gmail.com>
2023-11-23 16:30:36 -0500

    trail unit testing

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,5 +1,4 @@
 import unittest
-
 from app.controllers.quizzes_controller import QuizzesController
 
 class QuizzesTest(unittest.TestCase):
@@ -10,11 +9,29 @@ class QuizzesTest(unittest.TestCase):
         
     def test_expose_failure_01(self):
         """
-        Implement this function and two more that
-        execute the code and make it fail.
+        This test case exposes a failure in the code by trying to add a quiz with a missing title.
+        It is expected to crash at the line where the title is used to generate a quiz_id.
+        File: quizzes_controller.py, Line: 42
         """
-        self.assertTrue(True, 'Example assertion.')
-        
+        self.ctrl.add_quiz(None, 'Quiz Text', datetime.now(), datetime.now())
+
+    def test_expose_failure_02(self):
+        """
+        This test case exposes a failure in the code by trying to add a question to a non-existent quiz.
+        It is expected to crash at the line where the quiz object is accessed.
+        File: quizzes_controller.py, Line: 57
+        """
+        with self.assertRaises(Exception):  # Replace Exception with the actual exception type
+            self.ctrl.add_question('nonexistent_quiz_id', 'Question Title', 'Question Text')
+
+    def test_expose_failure_03(self):
+        """
+        This test case exposes a failure in the code by trying to add an answer to a non-existent question.
+        It is expected to crash at the line where the question object is accessed.
+        File: quizzes_controller.py, Line: 68
+        """
+        with self.assertRaises(Exception):  # Replace Exception with the actual exception type
+            self.ctrl.add_answer('nonexistent_question_id', 'Answer Text', True)
 
 if __name__ == '__main__':
     unittest.main()

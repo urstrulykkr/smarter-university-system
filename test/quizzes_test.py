5098e9f900a90e69fde8ec82abd9a89d2884c0b8
urstrulykkr <reddykaushik18@gmail.com>
2023-11-25 18:26:33 -0500

    Added unit-test cases

--- test/quizzes_test.py
+++ test/quizzes_test.py
@@ -1,37 +1,30 @@
 import unittest
 from app.controllers.quizzes_controller import QuizzesController
+from datetime import datetime
+
 
 class QuizzesTest(unittest.TestCase):
 
     def setUp(self):
-        # Run tests on non-production data
         self.ctrl = QuizzesController('quizzes_test.py')
         
     def test_expose_failure_01(self):
-        """
-        This test case exposes a failure in the code by trying to add a quiz with a missing title.
-        It is expected to crash at the line where the title is used to generate a quiz_id.
-        File: quizzes_controller.py, Line: 42
-        """
-        self.ctrl.add_quiz(None, 'Quiz Text', datetime.now(), datetime.now())
+            self.ctrl.add_quiz(None, 'Quiz-Text', datetime.now(), datetime.now())
 
     def test_expose_failure_02(self):
-        """
-        This test case exposes a failure in the code by trying to add a question to a non-existent quiz.
-        It is expected to crash at the line where the quiz object is accessed.
-        File: quizzes_controller.py, Line: 57
-        """
-        with self.assertRaises(Exception):  # Replace Exception with the actual exception type
-            self.ctrl.add_question('nonexistent_quiz_id', 'Question Title', 'Question Text')
+            self.ctrl.clear_data()
+            quiz_id = self.ctrl.add_quiz("Title: Quiz 1", "This is quiz text", datetime(2023, 1, 7, 11, 30, 0), datetime(2023, 11, 10, 19, 30, 0))
+            question_id = self.ctrl.add_question(quiz_id, "Title", "This is question text")
+            answer_id = self.ctrl.add_answer(question_id, "this is the answer", "This is Wrong type!")
+            self.assertEqual(len(answer_id),0,  "There must be zero quizzes.")
 
     def test_expose_failure_03(self):
-        """
-        This test case exposes a failure in the code by trying to add an answer to a non-existent question.
-        It is expected to crash at the line where the question object is accessed.
-        File: quizzes_controller.py, Line: 68
-        """
-        with self.assertRaises(Exception):  # Replace Exception with the actual exception type
-            self.ctrl.add_answer('nonexistent_question_id', 'Answer Text', True)
+        with self.assertRaises(Exception):
+            self.ctrl.add_question(-1, 'Question-Title', 'Question-Text')
+
 
 if __name__ == '__main__':
     unittest.main()
+
+
+
